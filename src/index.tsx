a6101bb2628465ca86f754691e2ff33493da133a
john3volta <workbox_1@tutanota.com>
2025-07-31 14:24:40 +0200

    add: svg sprite

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.scss';
 
+fetch('/sprite.svg')
+  .then(response => response.text())
+  .then(svgContent => {
+    const div = document.createElement('div');
+    div.innerHTML = svgContent;
+    div.style.display = 'none';
+    document.body.appendChild(div);
+  });
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
